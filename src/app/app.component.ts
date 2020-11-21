86f0450dc223b754c7ab002d4fd8001f380a7702
Asia Zioła <ziola.jm@gmail.com>
2020-11-21 16:02:44 +0100

    interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
-  todoList = [
+  todoList: TodoItem[] = [
     { title: 'install NodeJS' },
     { title: 'install Angular CLI' },
     { title: 'create new app' },
